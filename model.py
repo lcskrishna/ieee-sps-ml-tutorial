5872789fff0aed8d2f5531f4d4b2433202099322
lcskrishna <lollachaitanya@gmail.com>
2020-08-02 19:34:02 +0000

    create model sample

---
+++ model.py
@@ -0,0 +1,18 @@
+'''
+Creation of the CNN Network model for training MNIST dataset.
+Conv-> Pool->Conv->Pool->Relu(FC)->FC
+'''
+
+## Import statements.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MNISTNet(nn.Module):
+    def __init__(self):
+        # Please fill out the initialization of network here. 
+
+    def forward(self, x):
+        # Please fill out the forward function
+
+        return x
